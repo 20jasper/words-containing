f908f3d1ecd3f2fa4bab9a58b95a9146f78d9e50
Jacob Asper <jacobasper191@gmail.com>
2025-02-26 23:56:31 -0500

    make api endpoints

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.List (nub)
 import MyLib qualified (someFunc)
+import Web.Scotty
 
 main :: IO ()
-main = do
-  text <- lines <$> readFile "top-2500.txt"
-  print $ wordsWith "ailn" text
+main = scotty 3001 $ do
+  get "/" $ text "hello world"
+
+  get "/:letters" $ do
+    letters <- pathParam "letters"
+    words <- wordsWith letters <$> liftIO getWords
+    json words
+
+getWords :: IO [String]
+getWords = do
+  lines <$> readFile "top-2500.txt"
 
 wordsWith :: String -> [String] -> [String]
 wordsWith xs = filter (all (`elem` xs))
